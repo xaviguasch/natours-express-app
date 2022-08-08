68182843641d26184b83c02b418ccbef294f5f64
xaviguasch <xaviguasch@gmail.com>
2022-08-08 21:16:33 +0200

    added initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
