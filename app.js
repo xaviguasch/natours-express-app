3577ba0f10b345bfe8af95656d7d52e037f6d3c3
xaviguasch <xaviguasch@gmail.com>
2022-08-09 19:36:54 +0200

    toyed with creating our own middleware

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ const express = require('express');
 
 const app = express();
 
-// MIDDLEWARE. In this case, this is adding the data from the POST request to the body (of the request in an object, so in req.body)
+// MIDDLEWARE. In this case, this is giving us access to the data from the POST request in the body (the data is stored in an object,in req.body)
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('hello from the middleware!');
+
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
