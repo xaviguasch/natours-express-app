0d4ebaf1ea2937eab02ca19f64958b5f7a60572f
xaviguasch <xaviguasch@gmail.com>
2022-08-09 14:57:32 +0200

    configured /api/v1/tours route

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endcode...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
