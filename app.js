78de42633325fa0b26b646959a339b214edaa016
xaviguasch <xaviguasch@gmail.com>
2022-08-10 17:04:01 +0200

    more file organizing, added controllers folder and server.js file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
+const app = express();
 
 // 1)-MIDDLEWARES
 app.use(morgan('dev'));
@@ -27,8 +28,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4)- START SERVER
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1; // Converts the id string to a number
+
+  const tour = tours.find((tour) => tour.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    // we put "return" because we want to exit the function
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  // console.log(req.body);
+
+  // we're adding + 1 to the last id in the tours array.
+  const newId = tours[tours.length - 1].id + 1;
+
+  // Creating a new object with the data of the post request (in req.body) and the ID we just generated
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  // We're adding the newTour to the tours array
+  tours.push(newTour);
+
+  // We're replacing the old tours-simple.json file with a new one with the updated tours data.
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    // we put "return" because we want to exit the function
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  // We aren't really updating the tour item as this is just a demo app to familiarize us with Express
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    // we put "return" because we want to exit the function
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  // We aren't really deleting the tour item as this is just a demo app to familiarize us with Express
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
