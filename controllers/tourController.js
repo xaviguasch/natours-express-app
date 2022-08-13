0c4ed2857c374457fc414a46a2d0e0c73664267e
xaviguasch <xaviguasch@gmail.com>
2022-08-13 23:13:28 +0200

    updated documents for updateTour controller via PATCH route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -60,14 +60,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // We aren't really updating the tour item as this is just a demo app to familiarize us with Express
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
