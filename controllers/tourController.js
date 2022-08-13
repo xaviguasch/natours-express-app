17af16ce5b2d6732d1c2bd1c4fbd8b48a4ecc3ab
xaviguasch <xaviguasch@gmail.com>
2022-08-13 20:24:12 +0200

    created new documents via post request and stored it in mongodb

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res
-      .status(400)
-      .json({ status: 'fail', message: 'Missing name or price' });
-  }
-
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -35,13 +25,26 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  // const newTour = new Tour()
+  // newTour.save()
+
+  try {
+    const newTour = await Tour.create(req.body);
+    console.log(newTour);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
