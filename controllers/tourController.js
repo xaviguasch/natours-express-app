2e2c702654ad398d584000d78832b8f4f7cbab79
xaviguasch <xaviguasch@gmail.com>
2022-08-15 14:00:31 +0200

    added limiting fields implementation to the controller getAllTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,8 +23,15 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
-      console.log('testing');
       query = query.sort('-createdAt');
+    }
+
+    //  3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     // EXECUTE QUERY
