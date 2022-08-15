b8752991823f8ce7ce5de1ca5df59db78ba8f762
xaviguasch <xaviguasch@gmail.com>
2022-08-15 13:38:20 +0200

    added sorting to the controller getAllTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,18 +5,27 @@ exports.getAllTours = async (req, res) => {
     console.log(req.query);
 
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advanced filtering
+    // 1B) Advanced filtering
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     // This returns an object query, NOT an unresolved promise
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      console.log('testing');
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     // This returns an array with the objects representing the docuements found in the database
