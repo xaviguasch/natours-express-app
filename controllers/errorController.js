180c834766974433fb037c555ff96ac427a3ea90
xaviguasch <xaviguasch@gmail.com>
2022-08-27 23:55:31 +0200

    handled invalid database IDs errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,28 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    err: err,
+    error: err,
     message: err.message,
     stack: err.stack,
   });
@@ -15,26 +36,32 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     });
 
-    // Programming or other unknown error: don't want to leak error details to the client
+    // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
-    console.error('ERROR  🔥🔥🔥🔥', err);
+    console.error('ERROR 💥', err);
 
     // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Somehting went very wrong!',
+      message: 'Something went very wrong!',
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = Object.assign(err);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
