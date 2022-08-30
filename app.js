123e2470e56e75ae00e0cd5cc5f611173d840e00
xaviguasch <xaviguasch@gmail.com>
2022-08-30 16:33:18 +0200

    implemented part of PROTECT middleware to check on tokens

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(`${__dirname}/public`));
 // It adds the requestTime property to the req object
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-
+  console.log(req.headers);
   next();
 });
 
